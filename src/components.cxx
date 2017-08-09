dce3be6084fdb0ff5030316a7315d93ded20f854
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2017-08-09 15:39:09 +1000

    Components Initialisation
     * LanguageInfo repositories are now initialised as
       well

--- src/components.cxx
+++ src/components.cxx
@@ -1,10 +1,12 @@
 
-#include "components.hxx"
+#include <components.hxx>
 #include <utils/loader.hxx>
 #include <utils/component_factory.hxx>
+#include <fstrips/loader.hxx>
 
 fs0::Problem* generate(const rapidjson::Document& data, const std::string& data_dir) {
 	fs0::BaseComponentFactory factory;
+	fs0::fstrips::LanguageJsonLoader::loadLanguageInfo(data);
 	fs0::Loader::loadProblemInfo(data, data_dir, factory);
 	return fs0::Loader::loadProblem(data);
 }
--- src/components.hxx
+++ src/components.hxx
@@ -1,8 +1,11 @@
-
 #pragma once
 
 #include <cstring>
 #include <lib/rapidjson/document.h>
+#include <utils/loader.hxx>
+#include <utils/component_factory.hxx>
+#include <fstrips/loader.hxx>
+
 
 namespace fs0 { class Problem; }
 
